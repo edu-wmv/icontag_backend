d798357afea0351b975829f2c6a7fd961e3ae429
edu-wmv <montzdudu@outlook.com>
2023-08-24 15:40:29 -0300

    'set-fix'

--- pool.ts
+++ pool.ts
@@ -1,23 +1,14 @@
 require('dotenv').config();
 const mysql = require('mysql')
 
-const dbs: HostBase = {
-    "development": {
-        host: process.env.DB_HOST_DEV,
-        port: process.env.DB_PORT_DEV,  
-        user: process.env.DB_USER_DEV,
-        password: process.env.DB_PASS_DEV,
-        database: process.env.DB_NAME_DEV
-    },
-    "production": {
-        host: process.env.DB_HOST_PROD,
-        user: process.env.DB_USER_PROD,
-        password: process.env.DB_PASS_PROD,
-        database: process.env.DB_NAME_PROD,
-        port: process.env.DB_PORT_PROD  
-    }
+const development = {
+    host: process.env.DB_HOST_DEV,
+    port: process.env.DB_PORT_DEV,  
+    user: process.env.DB_USER_DEV,
+    password: process.env.DB_PASS_DEV,
+    database: process.env.DB_NAME_DEV
 }
 
-const pool = mysql.createPool(dbs[process.env.NODE_ENV || 'development'])
+const pool = mysql.createPool(development)
 
 module.exports = pool
