9601394215428a2408364cd08af2e293f7ce0878
edu-wmv <montzdudu@outlook.com>
2024-02-19 16:55:27 -0300

    refactoring

--- app.ts
+++ app.ts
@@ -1,30 +1,52 @@
-require('dotenv').config({ silent: true })
 import { Request, Response, NextFunction } from 'express'
+import { OkPacket, queryCallback } from 'mysql'
+import { v4 as uuidv4 } from 'uuid'
 
 const express = require("express")
-const app = express()
+const mysql = require('mysql')
 const logger = require('morgan')
-const db = require('./query')
-const port = process.env.PORT
+
+const app = express()
+const port = process.env.SERVER_PORT
+
+// DATABASE CONNECTION
+
+const development = {
+    host: process.env.DB_HOST_DEV,
+    port: process.env.DB_PORT_DEV,  
+    user: process.env.DB_USER_DEV,
+    password: process.env.DB_PASS_DEV,
+    database: process.env.DB_NAME_DEV
+}
+
+const pool = mysql.createPool(development)
+
+// QUERY FUNCTIONS
+
+
 
 app.use(logger('dev'))
 app.use(express.static('public'))
 app.use((req: Request, res: Response, next: NextFunction) => {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, API-Key")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     next()
 })
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-    const api_key = req.headers['api-key']
-    if (!api_key || api_key != process.env.API_KEY) {
-        res.status(401).send('Unauthorized')
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(403).json({ error: 'No token provided' });
+    }
+    else if (authHeader !== process.env.TOKEN) {
+        return res.status(401).json({ error: 'Not authorized' });
     } else {
-        next()
+        next();
     }
 })
 
-app.post("/insertData", db.insertData)
-app.post("/setPoint", db.setPoint)
+// app.post("/insertData", insertData)
+// app.post("/setPoint", setPoint)
 
 app.listen(port, () => { console.log(`⚡️[server]: Server is running on port ${port}`) })
